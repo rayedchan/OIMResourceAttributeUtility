ad169ea86b17dc98a4fb4574266791263ad196a7
rayedchan <rayedchan@hotmail.com>
2013-12-27 14:57:59 -0500

    Template for Welcome JFrame

--- src/project/rayedchan/swing/gui/WelcomeFrame.java
+++ src/project/rayedchan/swing/gui/WelcomeFrame.java
@@ -1,48 +1,56 @@
 package project.rayedchan.swing.gui;
 
-import java.awt.Cursor;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.Dimension;
+import java.awt.Insets;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 /**
  * @author rayedchan
  */
 public class WelcomeFrame extends JFrame
 {
-    JLabel exit;
-    JMenuBar mbar;
-    JLabel label;
-
     public WelcomeFrame()
     {
-        setTitle("Welcome");
-        setSize(400,400);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        mbar=new JMenuBar();
-        mbar.setLayout(new FlowLayout());
-
-        label=new JLabel("You are logged in.");
-        mbar.add(label);
-        exit=new JLabel("<html><a href=''>Exit</a></html>");
-        exit.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        exit.setFont(new Font("Tahoma",Font.PLAIN,13));
-
-        exit.addMouseListener(new MouseAdapter(){
-        public void mouseClicked(MouseEvent ae)
-        {
-        System.exit(0);
-        }
-        });
-
-        mbar.add(exit);
-        setJMenuBar(mbar);
-        setExtendedState(MAXIMIZED_BOTH);
+        initUI();
     }
+    
+    public final void initUI()
+    {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(new EmptyBorder(new Insets(40, 60, 40, 60)));
+             
+        //Panel Components
+        JButton lookupBtn = new JButton("Lookups");
+        JButton processFormFieldBtn = new JButton("Process Form Fields");
+        JButton processTaskBtn = new JButton("Process Tasks");
+        JButton reconFieldBtn = new JButton("Reconciliation Fields");
+        JButton rfToPffMappingBtn = new JButton("Recon Field - Form Field Mapping");
+        
+        //Add buttons to panel
+        panel.add(lookupBtn);
+        panel.add(Box.createRigidArea(new Dimension(0, 5)));
+        panel.add(processFormFieldBtn);
+        panel.add(Box.createRigidArea(new Dimension(0, 5)));
+        panel.add(processTaskBtn);
+        panel.add(Box.createRigidArea(new Dimension(0, 5)));
+        panel.add(reconFieldBtn);
+        panel.add(Box.createRigidArea(new Dimension(0, 5)));
+        panel.add(rfToPffMappingBtn);
+
+        add(panel); //Add panel to JFrame
+        pack();
+
+        //Set JFrame Properties
+        setTitle("Options");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    
 }
